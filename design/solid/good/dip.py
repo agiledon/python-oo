698ab4ae81e4f590e0cc30abf5d1cd7ce91e61dd
agiledon <15023157626@163.com>
2019-05-08 13:25:25 +0800

    add all solid examples

---
+++ design/solid/good/dip.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Here we solve the issues in the design in `python_code.bad.DIP` with an interface
+# (implemented with abstract class).
+
+from abc import ABCMeta, abstractmethod
+
+
+class Worker(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def work(self):
+        pass
+
+
+# `IWorker` defines a interface which requires `work` method.
+
+class NormalWorker(Worker):
+    def work(self):
+        print("I'm working!!")
+
+
+class Manager(object):
+    def __init__(self):
+        self.worker = None
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Worker), '`worker` must be of type {}'.format(NormalWorker)
+
+        self.worker = worker
+
+    def manage(self):
+        if self.worker is not None:
+            self.worker.work()
+            # And some complex codes go here....
+
+
+class SuperWorker(Worker):
+    def work(self):
+        print("I work very hard!!!")
+
+
+# Now, the manager support `SuperWorker`...
+# In addition, it will support any worker which obeys the interface defined by `IWorker`!
+
+def main():
+    worker = NormalWorker()
+    manager = Manager()
+    manager.set_worker(worker)
+    manager.manage()
+
+    # The following will not work...
+    super_worker = SuperWorker()
+    try:
+        manager.set_worker(super_worker)
+        manager.manage()
+    except AssertionError:
+        print("manager fails to support super_worker....")
+
+
+if __name__ == "__main__":
+    main()
